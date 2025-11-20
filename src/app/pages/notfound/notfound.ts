a32131befeacf81679ce5e2b9d308f35523a0083
Fazekas Róbert <fazekasrobert08@gmail.com>
2025-11-20 13:45:55 +0100

    Base pages generated, color palette defined, 404 page

--- src/app/pages/notfound/notfound.ts
+++ src/app/pages/notfound/notfound.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './notfound.html',
   styleUrl: './notfound.scss',
 })
