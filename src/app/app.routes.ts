ac01c2a7edf1c9918e122714d80e70e32f49de5c
Fazekas Róbert <fazekasrobert08@gmail.com>
2025-11-20 14:01:31 +0100

    Routing added for the basic pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
         path: 'products',
         loadComponent: () => import('./pages/products/products').then(m => m.Products)
     },
-    //{ path: '', redirectTo: 'home', pathMatch: 'full' }, 
+    {
+        path: 'cart',
+        loadComponent: () => import('./pages/cart/cart').then(m => m.Cart)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./pages/profile/profile').then(m => m.Profile)
+    },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
     {
         path: '**',
         loadComponent: () => import('./pages/notfound/notfound').then(m => m.Notfound)
