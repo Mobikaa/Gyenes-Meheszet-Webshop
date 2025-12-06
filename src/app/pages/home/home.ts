9605cd8cc9fe38200f27dbb103fc4569fbb0624c
Fazekas Róbert <fazekasrobert08@gmail.com>
2025-12-06 15:15:19 +0100

    producs layout changed form 3 to 4, home page half done

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Product } from '../../shared/models/product';
+import { ProductList } from '../../shared/constants/products_examples';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from "@angular/material/card";
+import { NumberSpacerPipe } from "../../pipes/number-spacer-pipe";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatIcon,
+    MatCardModule,
+    NumberSpacerPipe
+],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  products: Product[] = ProductList;
 
+  featuredList(id1: number, id2: number, id3: number, id4: number): Product[] {
+    const ids = [id1, id2, id3, id4];
+
+    return this.products.filter(x => ids.includes(x.id));
+  }
 }
