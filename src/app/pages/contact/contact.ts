7b879e0a3c881cab381f49d22b262af9c057a276
Fazekas Róbert <fazekasrobert08@gmail.com>
2025-12-07 21:29:49 +0100

    contact page, but im not satisfied

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
